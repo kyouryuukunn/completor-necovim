4f26600f996e07aeef46d1c584ed70b5ec7269e9
kyouryuukunn <akakyouryuu@gmail.com>
2018-10-08 14:58:26 +0900

    start completion at g:

--- pythonx/completor_vim.py
+++ pythonx/completor_vim.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import logging
+import logging, re
 from completor import Completor, vim, get_encoding
 from completor.compat import to_bytes
 
@@ -8,22 +8,28 @@ _cache = {}
 
 logger = logging.getLogger('completor')
 
-#TODO
-# baseの指定(:, _)
-# triggerの指定(:, _)
-# fuzzymatch
 class Necovim(Completor):
-    filetype = 'necovim'
+    filetype = 'vim'
     sync = True
-    trigger = r"""[a-zA-Z0-9#:_]+$"""
-    ident = r"""[a-zA-Z0-9#:_]+$"""
+    trigger = re.compile(r'[a-zA-Z0-9#:_]{2,}$', re.U | re.X)
 
     def parse(self, base):
         if not self.ft or not base:
             return []
-        start = self.start_column()
-        kw = self.input_data[start:]
 
+        logger.info('start necovim parse: %s', base)
+
+        try:
+            match = self.trigger.search(base)
+        except TypeError as e:
+            logger.exception(e)
+            match = None
+
+        if not match:
+            logger.info('no matches')
+            return []
+
+        kw = match.group()
         gather_candidates = vim.Function('necovim#gather_candidates')
         binput_data = to_bytes(self.input_data, get_encoding())
         bkw = to_bytes(kw, get_encoding())
@@ -35,9 +41,10 @@ class Necovim(Completor):
         } for item in gather_candidates(binput_data, bkw)[:]
               if item[b'word'].startswith(kw.encode('utf-8'))]
 
-        index = kw.rfind(base)
-        if index > 0:
-            prefix = len(kw[:index])
+        index = base.rfind(kw)
+        start_column = self.start_column()
+        prefix = start_column - index
+        if prefix > 0:
             for c in candidates:
                 c['abbr'] = c['word']
                 c['word'] = c['word'][prefix:]
